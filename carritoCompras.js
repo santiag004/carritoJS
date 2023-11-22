a0f5f3060f849015d17f2b66d1bbc88bc7ad60d2
marianoruiza <marianoruiz145@gmail.com>
2023-11-22 08:59:33 -0300

    Array con productos

---
+++ carritoCompras.js
@@ -0,0 +1,22 @@
+// Crear un carrito de compras. 
+
+// ■ Crear una aplicación de carrito de compras donde se ingresen
+
+// productos como elementos en un array.
+
+// ■ las tareas que debe hacer son las siguientes:
+
+// ● Agregar producto al carrito
+
+// ● Listar los productos del carrito
+
+// ● Buscar un producto en el carrito
+
+// ● Filtrar productos que coincidan con una palabra, parte de ella
+
+// o una letra
+
+// ● Eliminar producto del carrito
+
+let productos = ['Remera', 'Anteojos', 'Gorra', 'Short', 'Zapatillas'];
+console.log("Los producots que puede elegir son: " + productos);
