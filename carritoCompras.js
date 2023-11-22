d47e5ad0e9f756894c8d694d2732eae955dc911b
JavierRodriguez3 <javrodriguez2003@gmail.com>
2023-11-22 09:46:14 -0300

    ass

--- carritoCompras.js
+++ carritoCompras.js
@@ -14,3 +14,8 @@ const listarProductos = (array) =>{
 
 listarProductos(array)
 
+const borrarProductos = (array) =>{
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i])
+    }
+}
