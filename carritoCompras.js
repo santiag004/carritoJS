89d1dcdb02d75f4c810dc09fde0bbc27ecdf7a01
Santiago Soraire <eneasvallejo04@gmail.com>
2023-11-22 09:07:41 -0300

    segundo commit

--- carritoCompras.js
+++ carritoCompras.js
@@ -2,4 +2,5 @@ let carritoCompras = ["Elemento1", "Elemento2", "Elemento3"]
 
 const agregarElemento = (array, elemento) => {
     array.push(elemento)
+    console.log("El elemento " + elemento + " se ha eliminado del carrito")
 }
