8d4a5a520b3b2cda9d509649a24b86fefedee826
Santiago Soraire <eneasvallejo04@gmail.com>
2023-11-22 09:49:59 -0300

    merge 3

--- carritoCompras.js
+++ carritoCompras.js
@@ -14,3 +14,16 @@ const listarProductos = (array) =>{
 }
 
 listarProductos(array)
+
+// Función para buscar un producto en el carrito
+
+function buscarProducto(consulta) {
+    return carrito.find(producto => producto.includes(consulta));   
+  }
+
+
+// Función para filtrar productos en el carrito
+
+function filtrarProductos(consulta) {
+    return carrito.filter(producto => producto.includes(consulta));
+  }
